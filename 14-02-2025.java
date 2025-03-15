bdc608839da3d07e224f7e1bb9db16c1eeae1ed8
Shubham Raj Sharma <132924110+shubh100802@users.noreply.github.com>
2025-03-15 21:53:44 +0530

    14-02-2025.java

--- 14-02-2025.java
+++ 14-02-2025.java
@@ -3,23 +3,24 @@ import java.sql.*;
 public class JDBC{
   public static void main(String[] args){
     try{
-      Class.forName("com.mysql.jdbc.Driver");  // Register    // 1
-      
+      Class.forName("com.mysql.cj.jdbc.Driver");  // Register    // 1
+      System.out.println("11111)
       Connection con = DriverManager.getConnection(
-        "jdbc:mysql: // localhost:3306 / MyWish" , "root","root");
+        "jdbc:mysql: // localhost:3306 /friday" , "root","root");
       //here sonoo is database name, root is username and password     // 2
-
-    Statement stmt = con.createStatement();   // 3
-    ResultSet rs = stmt.executeQuery("select * from emp");  // 4
-
-    while(rs.next())
-    System.out.println(rs.getInt(1)+ " " + rs.getString + " " + rs.getString(3);
-
-    con.close();   //5
-
-    }
+      System.out.println("22222");
+      Statement stmt = con.createStatement();   // 3
+      System.out.println("33333");  
+      ResultSet rs = stmt.executeQuery("select * from student");  // 4
+  
+      while(rs.next())
+      System.out.println(rs.getInt(1)+ " " + rs.getString + " " + rs.getString(3);
+      }
+      con.close();   //5
+  
+      }
     catch(Exception e){
-      System.out.println("went wrong");
+      System.out.println("Shubham check your program, there is a small error");
     }
   }
 }
